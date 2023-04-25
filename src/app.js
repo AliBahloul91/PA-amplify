5749a4f5be6cad1393db0bbda5ef5fcd8892b9ee
AliBahloul91 <alinet1991@gmail.com>
2023-04-26 00:05:28 +0200

    fix build

--- src/app.js
+++ src/app.js
@@ -1,27 +1,17 @@
 import { Amplify, Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
-import { Amplify } from 'aws-amplify';
+
 
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+//import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-function App({ signOut, user }) {
-  return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
-  );
-}
-
 export default withAuthenticator(App);
-import { Amplify, API, graphqlOperation } from "aws-amplify";
-
- import awsconfig from "./aws-exports";
+import {  API, graphqlOperation } from "aws-amplify";
+ 
  import { createTodo } from "./graphql/mutations";
  import { listTodos } from "./graphql/queries";
  import { onCreateTodo } from "./graphql/subscriptions";
